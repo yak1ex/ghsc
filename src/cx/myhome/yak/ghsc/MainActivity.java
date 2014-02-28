6551871c0a873643a44341028fb0d504939e412d
Yak! <yak_ex@mx.scn.tv>
2014-03-01 01:31:32 +0900

    Add an activity for settings.

--- src/cx/myhome/yak/ghsc/MainActivity.java
+++ src/cx/myhome/yak/ghsc/MainActivity.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.Menu;
 import android.widget.TextView;
@@ -71,6 +73,16 @@ public class MainActivity extends Activity implements Handler.Callback {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.action_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		}
+		return false;
 	}
 
 	class RequestRunnable implements Runnable
