fd299cd178d42901a243e0698bf8007d5eead602
Yak! <yak_ex@mx.scn.tv>
2014-04-05 07:24:57 +0900

    Adjust to fit slight changes of GitHub site.

--- src/cx/myhome/yak/ghsc/MainActivity.java
+++ src/cx/myhome/yak/ghsc/MainActivity.java
@@ -167,10 +167,10 @@ public class MainActivity extends Activity implements Handler.Callback {
 			try {
 				Document d = Jsoup.connect("https://github.com/" + mAccount).get();
 				String s = d.getElementsByAttributeValueContaining("class", "contrib-streak-current").text();
-				Pattern p = Pattern.compile("(\\d+) days (\\w+) (\\d+) - (\\w+) (\\d+) Current Streak");
+				Pattern p = Pattern.compile("(\\d+) days (\\w+) (\\d+) - (\\w+) (\\d+) Current [Ss]treak");
 				Matcher m = p.matcher(s);
 				boolean matches = m.find(); 
-				if(!matches && !s.equals("0 days Rock - Hard Place Current Streak")) {
+				if(!matches && !s.equals("0 days Rock - Hard Place Current [Ss]treak")) {
 					ret.error = "Unknown string gotten: " + s;
 					ret.error_kind = Error.UNKNOWN_ERROR;
 					ret.success = false;
